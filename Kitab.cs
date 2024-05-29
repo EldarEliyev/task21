e1c285978cc3dd2c4acea4bf87e0d3aeaca3ac4e
eldar23451 <liyev8024@gmail.com>
2024-05-29 12:45:36 +0400

    task file is added

---
+++ Kitab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+  public   class Kitab 
+    {
+        public string bookname;
+        public string writer;
+        public int page;
+        public Kitab( string bookName)
+        {
+            this.bookname = bookName;  
+        }
+        public Kitab( int page,string writer)
+        {
+            this.page = page;
+            this.writer = writer;   
+        } 
+     public void  Getbookname()
+        {
+            Console.WriteLine($"Bookname{bookname}");
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp48;
+
+Kitab kitab = new Kitab("fyodor") ;
+kitab.Getbookname();
+
